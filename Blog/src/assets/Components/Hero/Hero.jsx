00191cb43823d7ff9da4e51de167e078adaf5aae
Yll Kelmendi <yllkelmendi05@gmail.com>
2024-04-30 17:59:12 +0200

    new file:   Blog/public/Images/word-cloud.svg 	modified:   Blog/src/assets/Components/Hero/Hero.jsx 	modified:   Blog/src/assets/Components/Hero/Hero.module.css 	modified:   Blog/src/assets/Components/Navbar/Navbar.module.css 	modified:   Blog/src/index.css

--- Blog/src/assets/Components/Hero/Hero.jsx
+++ Blog/src/assets/Components/Hero/Hero.jsx
@@ -9,12 +9,20 @@ function Hero () {
 
         <div className= {styles.heroContent}>
 
-          <h1 className={styles.heroTitle}>{"Ylli's <Code/> Blog"}</h1>
-          <Link to={"/Blog/"} className = {styles.heroButton}><p>View Blog</p></Link>
+          <h1 className={styles.heroTitle}>Ylli's <em>{"<Code/>"}</em> Blog</h1>
+          <Link to={"/Blog/"} className = {styles.heroButton}><p>View Blog</p>          
+          </Link>
 
         </div>  
 
+
+        <img src="../../../public/Images/word-cloud.svg" alt="Word Cloud" className={styles.wordCloud} />
+        <div className={styles.wordGlow}></div>
+
       </section>
+
+
+      <div style={{position: "relative", width: "100%", height: "0.1px"}}></div>
 
 
       <Outlet/>
