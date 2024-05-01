875371b2b29e53c47960b731b8292e00801cc89a
Yll Kelmendi <yllkelmendi05@gmail.com>
2024-05-01 14:23:39 +0200

    new file:   Blog/public/Images/css_logo.svg 	new file:   Blog/public/Images/html_logo.svg 	new file:   Blog/public/Images/javascript_logo.svg 	new file:   Blog/public/Images/react_logo.svg 	modified:   Blog/src/assets/Components/RecentBlogs/RecentBlogs.jsx 	modified:   Blog/src/assets/Components/RecentBlogs/RecentBlogs.module.css

--- Blog/src/assets/Components/RecentBlogs/RecentBlogs.jsx
+++ Blog/src/assets/Components/RecentBlogs/RecentBlogs.jsx
@@ -10,7 +10,49 @@ function RecentBlogs () {
       <section className={styles.recentBlogs}>
 
 
-          <h4 className={styles.recentBlogsTitle}>Recent Blogs</h4>
+          <h3 className={styles.recentBlogsTitle}>Recent Blogs</h3>
+
+
+          <div className={styles.cardsParent}>
+
+
+            <Link to={"/Blog/"} className={styles.articleLink}>  
+              <div className={styles.articleCard}>
+                
+
+                <div className={styles.articleContent}>
+
+                  <img src="/Images/html_logo.svg" alt="Blog Logo Coding" className={styles.articleLogo}/>
+                  <h6 className={styles.articleTitle}>
+                    How to start web development as a beginner?
+                  </h6>
+
+                </div>
+
+
+              </div>
+            </Link>
+            
+
+
+            <Link to={"/Blog/"} className={styles.articleLink}>  
+              <div className={styles.articleCard}>
+
+              <img src="/Images/react_logo.svg" alt="Blog Logo Coding" className={styles.articleLogo}/>
+
+              </div>
+            </Link>
+
+
+            <Link to={"/Blog/"} className={styles.articleLink}>  
+              <div className={styles.articleCard}>
+
+              <img src="/Images/javascript_logo.svg" alt="Blog Logo Coding" className={styles.articleLogo}/>
+
+              </div>
+            </Link>
+
+          </div>
 
 
       </section>
