22555ce34d1059d51cc5629512e0e308cae55a7a
Yll Kelmendi <yllkelmendi05@gmail.com>
2024-05-03 14:21:43 +0200

    deleted:    Blog/src/assets/Components/RecentBlogs/RecentBlogs.jsx 	deleted:    Blog/src/assets/Components/RecentBlogs/RecentBlogs.module.css 	modified:   Blog/src/assets/Pages/Home.jsx 	modified:   Blog/src/index.css

--- Blog/src/assets/Pages/Home.jsx
+++ Blog/src/assets/Pages/Home.jsx
@@ -1,6 +1,6 @@
 import Navbar from "../Components/Navbar/Navbar";
 import Hero from "../Components/Hero/Hero";
-import RecentBlogs from "../Components/RecentBlogs/RecentBlogs";
+import Article from "../Components/Article/Article";
 
 function Home () {
   return (
@@ -12,7 +12,26 @@ function Home () {
       {/* Header End */}
 
 
-      <RecentBlogs/>
+      {/* Recent Blogs */}
+        <section className={"recentBlogs"}>
+
+
+            <h3 className={"recentBlogsTitle"}>Recent Blogs</h3>
+
+
+            <div className={"cardsParent"}>
+
+              <Article title = {"How to start web development as a beginner?"} logoSrc = {"/Images/html_logo.svg"} link = {"How_to_start_web_development_as_a_beginner"}/>
+              <Article title = {"How to create an accordion with React JS?"} logoSrc = {"/Images/react_logo.svg"} link = {"How_to_create_an_accordion_with_React_JS"}/>
+              <Article title = {"CSS Basics"} logoSrc = {"/Images/css_logo.svg"} link = {"CSS_Basics"}/>
+
+            </div>
+
+            <div style={{width: "100%", height: "1px", position: "relative"}}></div>
+
+
+        </section>
+      {/* Recent Blogs End */}
 
     </>
   )
