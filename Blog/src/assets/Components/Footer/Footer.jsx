f16f78ccae918db24753a9b35e8a03086d57fc89
Yll Kelmendi <yllkelmendi05@gmail.com>
2024-05-04 10:13:30 +0200

    modified:   Blog/src/assets/Components/Footer/Footer.jsx 	modified:   Blog/src/assets/Components/Footer/Footer.module.css

--- Blog/src/assets/Components/Footer/Footer.jsx
+++ Blog/src/assets/Components/Footer/Footer.jsx
@@ -5,49 +5,56 @@ function Footer () {
   return (
     <>
     <div className={styles.footerParent}>
+      <div className={styles.footerOutline}>
 
 
-      <h5 className={styles.footerTitle}>Ylli's Blog</h5>
+        <h5 className={styles.footerTitle}><em>-</em> Ylli's Blog <em>-</em></h5>
 
-      <ul className={styles.footerUl}>
-        <li className={styles.footerHeader}>
-          <h6>Nav</h6>
-        </li>
+        <ul className={styles.footerUl}>
+          <li>
+            <h6 className={styles.footerHeader}>Nav</h6>
+          </li>
 
-        <li><Link to={"/"} className={styles.footerLink}><p>Home</p></Link></li>
-        <li><Link to={"/Blog/"} className={styles.footerLink}><p>Blog</p></Link></li>
-        <li><Link to={"/Website_Inspo/"} className={styles.footerLink}><p>Inspo</p></Link></li>
-      </ul>
-
-
-
-
-      <ul className={styles.footerUl}>
-        <li className={styles.footerHeader}>
-          <h6>Contact</h6>
-        </li>
-
-        <li>
-          <Link to={"/"} className={styles.footerLink}><p>Contact Me</p></Link>
-        </li>
-        <li><small>yllkelmendi05@gmail.com</small></li>
-      </ul>
+          <li><Link to={"/"} className={styles.footerLink}><p>Home</p></Link></li>
+          <li><Link to={"/Blog/"} className={styles.footerLink}><p>Blog</p></Link></li>
+          <li><Link to={"/Website_Inspo/"} className={styles.footerLink}><p>Inspo</p></Link></li>
+        </ul>
 
 
 
 
-      <ul className={styles.footerUl}>
-        <li className={styles.footerHeader}>
-          <h6>Social</h6>
-        </li>
+        <ul className={styles.footerUl}>
+          <li>
+            <h6 className={styles.footerHeader}>Contact</h6>
+          </li>
 
-        <li>
-          <Link to={"/"} className={styles.footerLink}><p>LinkedIn</p></Link>
-        </li>
-        <li><Link to={"/"} className={styles.footerLink}><p>Fiverr</p></Link></li>
-      </ul>
+          <li>
+            <Link to={"/"} className={styles.footerLink}><p>Contact Me</p></Link>
+          </li>
+          <li><small>yllkelmendi05@gmail.com</small></li>
+        </ul>
 
-      
+
+
+
+        <ul className={styles.footerUl}>
+          <li>
+            <h6 className={styles.footerHeader}>Social</h6>
+          </li>
+
+          <li>
+            <Link to={"/"} className={styles.footerLink}><p>LinkedIn</p></Link>
+          </li>
+          <li><Link to={"/"} className={styles.footerLink}><p>Fiverr</p></Link></li>
+        </ul>
+
+
+      </div>
+
+      <div className={styles.footerCopyright}>
+        <small>&copy; Yll Kelmendi {new Date().getFullYear()}</small>
+      </div>
+
     </div>
 
   
