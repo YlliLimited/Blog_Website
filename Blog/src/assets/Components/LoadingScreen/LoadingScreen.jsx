d9c799a224fcf93137c5fa4d02c9acc5866e4a45
Yll Kelmendi <yllkelmendi05@gmail.com>
2024-05-04 20:19:36 +0200

    new file:   Blog/public/Fonts/Jersey15-Regular.ttf 	new file:   Blog/public/Fonts/Jersey15-Regular.woff 	modified:   Blog/public/Images/word-cloud.svg 	modified:   Blog/src/assets/Components/LoadingScreen/LoadingScreen.jsx 	modified:   Blog/src/index.css

--- Blog/src/assets/Components/LoadingScreen/LoadingScreen.jsx
+++ Blog/src/assets/Components/LoadingScreen/LoadingScreen.jsx
@@ -1,19 +1,49 @@
 import styles from "./LoadingScreen.module.css"
+import { useEffect, useRef } from "react";
 
 
 function LoadingScreen () {
 
+  const loadingScreen = useRef(null);
+
+
+  useEffect(() => {
+
+
+    function removeLoadingScreen() {
+      loadingScreen.current.style.visibility = "hidden";
+    }
+
+
+    if (document.readyState === 'complete') {
+
+      removeLoadingScreen();
+
+    } else {
+
+      window.addEventListener('load', removeLoadingScreen);
+
+
+      return () => window.removeEventListener('load', removeLoadingScreen);
+    }
+
+
+  }, []);
+
+
+
     return (
-        <div className={styles.loadingScreenBackground}>
-          <div className="loadingScreenContent">
+      <div className={styles.loadingScreenBackground} ref={loadingScreen}>
+        <div className="loadingScreenContent">
 
-            <img src="/Images/pusheenCoffe.gif" alt="Pusheen drinking coffe gif" className={styles.pusheenGif}/>
-            <h3 className={styles.loadingTitle}>Loading<em className={styles.loadingDot}>.</em><em className={styles.loadingDot}>.</em><em className={styles.loadingDot}>.</em></h3>
+          <img src="/Images/pusheenCoffe.gif" alt="Pusheen drinking coffe gif" className={styles.pusheenGif}/>
+          <h3 className={styles.loadingTitle}>Loading<em className={styles.loadingDot}>.</em><em className={styles.loadingDot}>.</em><em className={styles.loadingDot}>.</em></h3>
 
-          </div>
         </div>
+      </div>
     )
 
-}
+
+}   
 
 export default LoadingScreen;
