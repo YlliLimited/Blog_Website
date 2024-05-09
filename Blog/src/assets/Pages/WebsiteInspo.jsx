c8e526e717f47179b8ffbe755f4733332a7df035
Yll Kelmendi <yllkelmendi05@gmail.com>
2024-05-09 09:10:47 +0200

    new file:   Blog/public/Images/cybertruckScreenshot.png 	new file:   Blog/public/Images/cybertruck_logo.svg 	modified:   Blog/src/assets/Pages/WebsiteInspo.jsx 	modified:   Blog/src/index.css

--- Blog/src/assets/Pages/WebsiteInspo.jsx
+++ Blog/src/assets/Pages/WebsiteInspo.jsx
@@ -27,18 +27,45 @@ function WebsiteInspo () {
 
         <h3 className="inspoHeader"><em>- </em>Website Ispo<em> -</em></h3>
 
+        {/* Apple */}
         <section id="inspoSection">
-          <div className="inspoTitle" style={{color: "white", backgroundColor: "black"}}>
+          <div className="inspoTitle" style={{backgroundColor: "black", color: "white"}}>
             <h4>Apple</h4>
             <img src="/Images/apple_logo.svg" alt="Apple Logo svg" loading="lazy"/>
           </div>
 
-          <div className="inspoBody">  
+          <div className="inspoBody" style={{backgroundColor: "white", color: "black"}}>  
             <a href="https://apple.com" target="_blank">
               <img src="/Images/appleScreenshot.png" alt="Apple Iphone 15 Pro website header" className="inspoScreenshot"/>
             </a>
           </div>
         </section>
+        {/* Apple End */}
+
+
+
+        <div className="threeLinesParent">
+          <div className="line"></div>
+          <div className="line"></div>
+          <div className="line"></div>
+        </div>
+
+
+
+        {/* Tesla Cybertruck */}
+        <section id="inspoSection">
+          <div className="inspoTitle" style={{backgroundColor: "white", color: "black"}}>
+            <h4>Tesla Cybertruck</h4>
+            <img src="/Images/cybertruck_logo.svg" alt="Tesla Cybertruck Logo svg" loading="lazy"/>
+          </div>
+
+          <div className="inspoBody" style={{backgroundColor: "black", color: "white"}}>  
+            <a href="https://www.tesla.com/cybertruck" target="_blank">
+              <img src="/Images/cybertruckScreenshot.png" alt="Tesla Cybertruck Website Header" className="inspoScreenshot"/>
+            </a>
+          </div>
+        </section>
+        {/* Tesla Cybertruck End */}
 
 
 
