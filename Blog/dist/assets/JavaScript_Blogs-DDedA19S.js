31183144fc8e252872bed5620b64535d29254fd9
Yll Kelmendi <yllkelmendi05@gmail.com>
2024-05-12 15:27:07 +0200

    modified:   Blog/.gitignore 	new file:   Blog/dist/Fonts/Jersey15-Regular.ttf 	new file:   Blog/dist/Images/Languages_Map.png 	new file:   Blog/dist/Images/appleScreenshot.png 	new file:   Blog/dist/Images/apple_logo.svg 	new file:   Blog/dist/Images/css_logo.svg 	new file:   Blog/dist/Images/cybertruckScreenshot.png 	new file:   Blog/dist/Images/cybertruck_logo.svg 	new file:   Blog/dist/Images/html_logo.svg 	new file:   Blog/dist/Images/javascript_logo.svg 	new file:   Blog/dist/Images/notionScreenshot.png 	new file:   Blog/dist/Images/notion_logo.svg 	new file:   Blog/dist/Images/orangeYouGladScreenshot.png 	new file:   Blog/dist/Images/orangeYouGlad_logo.jpg 	new file:   Blog/dist/Images/pusheenCoffe.gif 	new file:   Blog/dist/Images/react_logo.svg 	new file:   Blog/dist/Images/webDev_logo.svg 	new file:   Blog/dist/Images/word-cloud.png 	new file:   Blog/dist/assets/Article-Bm78Vk7e.js 	new file:   Blog/dist/assets/Article-UjdH0_Ib.css 	new file:   Blog/dist/assets/ArticleChapterBar-BugJDY_i.css 	new file:   Blog/dist/assets/ArticleChapterBar-CDtfveOY.js 	new file:   Blog/dist/assets/Blog-Bo3PBzcR.js 	new file:   Blog/dist/assets/BlogsToLoad-BmYdwHBf.js 	new file:   Blog/dist/assets/CSS_Blogs-DoHaXBmv.js 	new file:   Blog/dist/assets/Contact-DdbK8NdO.js 	new file:   Blog/dist/assets/Footer-CN-qS_NL.js 	new file:   Blog/dist/assets/Footer-DmJKXmne.css 	new file:   Blog/dist/assets/HTML_Blogs-DoHaXBmv.js 	new file:   Blog/dist/assets/Helmet-CPsRexLD.js 	new file:   Blog/dist/assets/Home-BJ0ffMjR.js 	new file:   Blog/dist/assets/How_To_Start_Web_Development_as_a_beginner-CoTAYkEV.js 	new file:   Blog/dist/assets/JavaScript_Blogs-DDedA19S.js 	new file:   Blog/dist/assets/MainPageContent-Izu7XGI7.css 	new file:   Blog/dist/assets/MainPageContent-TCpr-yaU.js 	new file:   Blog/dist/assets/Navbar-C9ARFjJ9.css 	new file:   Blog/dist/assets/Navbar-CWeIp2oe.js 	new file:   Blog/dist/assets/Other_Blogs-BTlzwk5e.js 	new file:   Blog/dist/assets/Page404-DpmwNXFX.js 	new file:   Blog/dist/assets/React_Blogs-B-uNJ0oq.js 	new file:   Blog/dist/assets/RecentBlogs-MhqP9tJA.js 	new file:   Blog/dist/assets/RecentBlogs-sdaHu6OQ.css 	new file:   Blog/dist/assets/ThreeLines-mYKnWzfp.css 	new file:   Blog/dist/assets/ThreeLines-yy5cL-Vo.js 	new file:   Blog/dist/assets/WebsiteInspo-CoBM66dw.js 	new file:   Blog/dist/assets/index-CVZV_fHU.js 	new file:   Blog/dist/assets/index-SmJ63U51.css 	new file:   Blog/dist/index.html 	modified:   Blog/package.json

---
+++ Blog/dist/assets/JavaScript_Blogs-DDedA19S.js
@@ -0,0 +1,2 @@
+const __vite__fileDeps=["assets/Article-Bm78Vk7e.js","assets/index-CVZV_fHU.js","assets/index-SmJ63U51.css","assets/Article-UjdH0_Ib.css"],__vite__mapDeps=i=>i.map(i=>__vite__fileDeps[i]);
+import{j as r,r as t,_ as a}from"./index-CVZV_fHU.js";t.lazy(()=>a(()=>import("./Article-Bm78Vk7e.js"),__vite__mapDeps([0,1,2,3])));function e(){return r.jsx(r.Fragment,{})}export{e as default};
