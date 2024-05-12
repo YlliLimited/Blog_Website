eae69ef9b8d0e83b2a1a973de053099713ad107e
Yll Kelmendi <yllkelmendi05@gmail.com>
2024-05-12 14:37:55 +0200

    Updates

---
+++ assets/Other_Blogs-BTlzwk5e.js
@@ -0,0 +1,2 @@
+const __vite__fileDeps=["assets/Article-Bm78Vk7e.js","assets/index-CVZV_fHU.js","assets/index-SmJ63U51.css","assets/Article-UjdH0_Ib.css"],__vite__mapDeps=i=>i.map(i=>__vite__fileDeps[i]);
+import{j as e,r as t,_ as o}from"./index-CVZV_fHU.js";const r=t.lazy(()=>o(()=>import("./Article-Bm78Vk7e.js"),__vite__mapDeps([0,1,2,3])));function s(){return e.jsx(e.Fragment,{children:e.jsx(r,{title:"How to start web development as a beginner?",logoSrc:"webDev_logo.svg",link:"How_to_start_web_development_as_a_beginner"})})}export{s as default};
