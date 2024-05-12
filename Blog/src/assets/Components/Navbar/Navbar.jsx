06d19eb2dc125eecda5d2664ff2b7236c0f68cd7
Yll Kelmendi <yllkelmendi05@gmail.com>
2024-05-12 13:54:32 +0200

    modified:   Blog/src/assets/Components/Footer/Footer.module.css 	modified:   Blog/src/assets/Components/MainPageContent/MainPageContent.module.css 	modified:   Blog/src/assets/Components/Navbar/Navbar.jsx 	modified:   Blog/src/assets/Components/ArticleChapterBar/ArticleChapterBar.module.css 	modified:   Blog/src/assets/Components/Navbar/Navbar.module.css 	modified:   Blog/src/assets/Components/RecentBlogs/RecentBlogs.module.css 	modified:   Blog/src/assets/Components/ThreeLines/ThreeLines.module.css 	modified:   Blog/src/index.css

--- Blog/src/assets/Components/Navbar/Navbar.jsx
+++ Blog/src/assets/Components/Navbar/Navbar.jsx
@@ -34,31 +34,85 @@ function Navbar (props) {
   }
 
 
-  return (
-    <>
-    
-      <nav>
+  const offCanvas = useRef(null);
+  const [offCanvasStatus, setOffCanvasStatus] = useState("closed");
 
-        <div className={styles.navbar} ref={navbar}>
-          
+  function toggleOffcanvas() {
+    if (offCanvasStatus === "opened") {
+      offCanvas.current.style.left = "calc(0px - calc(var(--standard-padding) * 8) - 4px)";
+      offCanvas.current.style.boxShadow = "none";
+      setOffCanvasStatus("closed");
+    } else if (offCanvasStatus === "closed") {
+      offCanvas.current.style.left = "0px";
+      offCanvas.current.style.boxShadow = "0px 0px 20px var(--secondary-contrast-color)";
+      setOffCanvasStatus("opened");
+    }
+  }
 
-          <ul className={styles.navUl}>
-              <li className={styles.navbarTitle}><Link to={"/"}><p>Ylli's Blog</p></Link></li>
-              <li><Link to={"/"} className={styles.navLink}><p>Home</p></Link></li>
-              <li><Link to={"/Blog/"} className={styles.navLink}><p>Blog</p></Link></li>
-              <li><Link to={"/Website_Inspo/"} className={styles.navLink}><p>Website Inspo</p></Link></li>
-              <li><Link to={"/Contact/"} className={styles.navButton}><p>Contact Me</p></Link></li>
-          </ul>
 
 
-        </div>
+  if (window.innerWidth >= 1000) {
+    return (
+      <>
+      
+        <nav>
+  
+          <div className={styles.navbar} ref={navbar}>
+            
+  
+            <ul className={styles.navUl}>
+                <li className={styles.navbarTitle}><Link to={"/"}><p>Ylli's Blog</p></Link></li>
+                <li><Link to={"/"} className={styles.navLink}><p>Home</p></Link></li>
+                <li><Link to={"/Blog/"} className={styles.navLink}><p>Blog</p></Link></li>
+                <li><Link to={"/Website_Inspo/"} className={styles.navLink}><p>Website Inspo</p></Link></li>
+                <li><Link to={"/Contact/"} className={styles.navButton}><p>Contact Me</p></Link></li>
+            </ul>
+  
+  
+          </div>
+  
+        </nav>
+  
+        <Outlet/>
+      
+      </>
+    )
+  } else if (window.innerWidth <= 999.98){
+    return(
+      <>
+      
+        <nav>
+  
+          <div className={styles.navbar} ref={navbar}>
+            
+  
+            <ul className={styles.navUl}>
+                <li className={styles.navbarTitle}><Link to={"/"}><p>Ylli's Blog</p></Link></li>
+                <li><button onClick={toggleOffcanvas} className={styles.navbarToggler}><p>|||</p></button></li>
+            </ul>
+  
+  
+          </div>
 
-      </nav>
 
-      <Outlet/>
-    
-    </>
-  )
+          <div className={styles.offCanvas} ref={offCanvas}>
+            <ul className={styles.navUl}>
+                <li><h6>Navigation</h6></li>
+                <hr className={styles.offCanvasHr}/>
+                <li><Link to={"/"} className={styles.navLink}><p>Home</p></Link></li>
+                <li><Link to={"/Blog/"} className={styles.navLink}><p>Blog</p></Link></li>
+                <li><Link to={"/Website_Inspo/"} className={styles.navLink}><p>Website Inspo</p></Link></li>
+                <li><Link to={"/Contact/"} className={styles.navButton}><p>Contact Me</p></Link></li>
+            </ul>
+          </div>
+  
+        </nav>
+  
+        <Outlet/>
+      
+      </>
+    )
+  }
 
 }
 
