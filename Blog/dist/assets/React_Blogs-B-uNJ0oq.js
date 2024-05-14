40994da7a22f4ddcd58dead9b7a1c9915dfd3283
Yll Kelmendi <yllkelmendi05@gmail.com>
2024-05-14 13:37:31 +0200

    modified:   Blog/dist/Images/Languages_Map.png 	modified:   Blog/dist/Images/appleScreenshot.png 	modified:   Blog/dist/Images/cybertruckScreenshot.png 	modified:   Blog/dist/Images/notionScreenshot.png 	modified:   Blog/dist/Images/orangeYouGladScreenshot.png 	modified:   Blog/dist/Images/orangeYouGlad_logo.jpg 	modified:   Blog/dist/Images/word-cloud.png 	renamed:    Blog/dist/assets/Article-Bm78Vk7e.js -> Blog/dist/assets/Article-vax4omEO.js 	renamed:    Blog/dist/assets/ArticleChapterBar-CDtfveOY.js -> Blog/dist/assets/ArticleChapterBar-C32MLbz8.js 	renamed:    Blog/dist/assets/Blog-Bo3PBzcR.js -> Blog/dist/assets/Blog-C15SpZ72.js 	new file:   Blog/dist/assets/BlogsToLoad-B-B36i_N.js 	deleted:    Blog/dist/assets/BlogsToLoad-BmYdwHBf.js 	new file:   Blog/dist/assets/CSS_Blogs-CJzETv7I.js 	deleted:    Blog/dist/assets/CSS_Blogs-DoHaXBmv.js 	renamed:    Blog/dist/assets/Contact-DdbK8NdO.js -> Blog/dist/assets/Contact-ChiYvGYZ.js 	renamed:    Blog/dist/assets/Footer-CN-qS_NL.js -> Blog/dist/assets/Footer-DRAvvSDX.js 	new file:   Blog/dist/assets/HTML_Blogs-CJzETv7I.js 	deleted:    Blog/dist/assets/HTML_Blogs-DoHaXBmv.js 	renamed:    Blog/dist/assets/Helmet-CPsRexLD.js -> Blog/dist/assets/Helmet-DghuNgXv.js 	renamed:    Blog/dist/assets/Home-BJ0ffMjR.js -> Blog/dist/assets/Home--52pqt8u.js 	renamed:    Blog/dist/assets/How_To_Start_Web_Development_as_a_beginner-CoTAYkEV.js -> Blog/dist/assets/How_To_Start_Web_Development_as_a_beginner-DTt5VTH-.js 	deleted:    Blog/dist/assets/JavaScript_Blogs-DDedA19S.js 	new file:   Blog/dist/assets/JavaScript_Blogs-DJoAiC3c.js 	renamed:    Blog/dist/assets/MainPageContent-TCpr-yaU.js -> Blog/dist/assets/MainPageContent-DNZtBpsZ.js 	deleted:    Blog/dist/assets/Navbar-C9ARFjJ9.css 	renamed:    Blog/dist/assets/Navbar-CWeIp2oe.js -> Blog/dist/assets/Navbar-D-I4mXFw.js 	new file:   Blog/dist/assets/Navbar-DZEUhCjd.css 	deleted:    Blog/dist/assets/Other_Blogs-BTlzwk5e.js 	new file:   Blog/dist/assets/Other_Blogs-BlK5kObu.js 	renamed:    Blog/dist/assets/Page404-DpmwNXFX.js -> Blog/dist/assets/Page404-BLqd8JP7.js 	deleted:    Blog/dist/assets/React_Blogs-B-uNJ0oq.js 	new file:   Blog/dist/assets/React_Blogs-CuFuBRKB.js 	new file:   Blog/dist/assets/RecentBlogs-BZjidf8o.js 	deleted:    Blog/dist/assets/RecentBlogs-MhqP9tJA.js 	renamed:    Blog/dist/assets/ThreeLines-yy5cL-Vo.js -> Blog/dist/assets/ThreeLines-B7idonQh.js 	renamed:    Blog/dist/assets/WebsiteInspo-CoBM66dw.js -> Blog/dist/assets/WebsiteInspo-CpxhhRbJ.js 	renamed:    Blog/dist/assets/index-SmJ63U51.css -> Blog/dist/assets/index-DlJ8IvQF.css 	renamed:    Blog/dist/assets/index-CVZV_fHU.js -> Blog/dist/assets/index-vE61zxHJ.js 	modified:   Blog/dist/index.html 	modified:   Blog/index.html

--- Blog/dist/assets/React_Blogs-B-uNJ0oq.js
+++
@@ -1,2 +0,0 @@
-const __vite__fileDeps=["assets/Article-Bm78Vk7e.js","assets/index-CVZV_fHU.js","assets/index-SmJ63U51.css","assets/Article-UjdH0_Ib.css"],__vite__mapDeps=i=>i.map(i=>__vite__fileDeps[i]);
-import{j as t,r,_ as a}from"./index-CVZV_fHU.js";r.lazy(()=>a(()=>import("./Article-Bm78Vk7e.js"),__vite__mapDeps([0,1,2,3])));function o(){return t.jsx(t.Fragment,{})}export{o as default};
