f1e3778354ccfd36af082efc4cc1ae0e04b454fc
Yll Kelmendi <yllkelmendi05@gmail.com>
2024-05-14 20:19:07 +0200

    renamed:    Blog/dist/assets/Article-T_0p32mL.js -> Blog/dist/assets/Article-3xyu56yz.js 	deleted:    Blog/dist/assets/ArticleChapterBar-BugJDY_i.css 	new file:   Blog/dist/assets/ArticleChapterBar-CQdOMjnW.css 	new file:   Blog/dist/assets/ArticleChapterBar-CWJnE6c-.js 	deleted:    Blog/dist/assets/ArticleChapterBar-DdHknkUb.js 	renamed:    Blog/dist/assets/Blog-D7CJbrqF.js -> Blog/dist/assets/Blog-_-mlWFzI.js 	renamed:    Blog/dist/assets/BlogsToLoad-WrDzjpgn.js -> Blog/dist/assets/BlogsToLoad-D8gqZ6M2.js 	deleted:    Blog/dist/assets/CSS_Blogs-BoOqUbSG.js 	new file:   Blog/dist/assets/CSS_Blogs-CEMyAQsO.js 	renamed:    Blog/dist/assets/Contact-BxRaU3Qr.js -> Blog/dist/assets/Contact-CU-nNkJp.js 	renamed:    Blog/dist/assets/Footer-D0fNDau0.js -> Blog/dist/assets/Footer-B_9wdJrP.js 	deleted:    Blog/dist/assets/HTML_Blogs-BoOqUbSG.js 	new file:   Blog/dist/assets/HTML_Blogs-CEMyAQsO.js 	renamed:    Blog/dist/assets/Helmet-BEkUQCy8.js -> Blog/dist/assets/Helmet-3FZyJYeu.js 	renamed:    Blog/dist/assets/Home-1NvpTNOL.js -> Blog/dist/assets/Home-DhOyoMnR.js 	renamed:    Blog/dist/assets/How_To_Start_Web_Development_as_a_beginner-Cv0RIEuX.js -> Blog/dist/assets/How_To_Start_Web_Development_as_a_beginner-N_vqRrYX.js 	deleted:    Blog/dist/assets/JavaScript_Blogs-D662JL4i.js 	new file:   Blog/dist/assets/JavaScript_Blogs-DSHgTU0v.js 	new file:   Blog/dist/assets/MainPageContent-BsuY-wZo.css 	renamed:    Blog/dist/assets/MainPageContent-BNYd1Cfr.js -> Blog/dist/assets/MainPageContent-ChA93lgQ.js 	deleted:    Blog/dist/assets/MainPageContent-Izu7XGI7.css 	renamed:    Blog/dist/assets/Navbar-CWEGWUyY.js -> Blog/dist/assets/Navbar-C_c6DEt2.js 	renamed:    Blog/dist/assets/Other_Blogs-C7jDXiDw.js -> Blog/dist/assets/Other_Blogs-BX-G_MZR.js 	renamed:    Blog/dist/assets/Page404-BP6_0wLD.js -> Blog/dist/assets/Page404-CC7BJzEV.js 	new file:   Blog/dist/assets/React_Blogs-8-Bhydjs.js 	deleted:    Blog/dist/assets/React_Blogs-BK_1D5yP.js 	new file:   Blog/dist/assets/RecentBlogs-ByfdxHa6.js 	deleted:    Blog/dist/assets/RecentBlogs-CN4V9rhB.js 	new file:   Blog/dist/assets/RecentBlogs-D2rH45Ds.css 	deleted:    Blog/dist/assets/RecentBlogs-sdaHu6OQ.css 	renamed:    Blog/dist/assets/ThreeLines-8AUlO-_B.js -> Blog/dist/assets/ThreeLines-tMRlYI-H.js 	renamed:    Blog/dist/assets/WebsiteInspo-BokBBMsp.js -> Blog/dist/assets/WebsiteInspo-QL-B-WSN.js 	renamed:    Blog/dist/assets/index-CM1kpqyO.js -> Blog/dist/assets/index-DhMDlh6k.js 	renamed:    Blog/dist/assets/index-CXg3SZqi.css -> Blog/dist/assets/index-ravPhIF3.css 	modified:   Blog/dist/index.html

---
+++ Blog/dist/assets/HTML_Blogs-CEMyAQsO.js
@@ -0,0 +1,2 @@
+const __vite__fileDeps=["assets/Article-3xyu56yz.js","assets/index-DhMDlh6k.js","assets/index-ravPhIF3.css","assets/Article-UjdH0_Ib.css"],__vite__mapDeps=i=>i.map(i=>__vite__fileDeps[i]);
+import{j as r,r as e,_ as s}from"./index-DhMDlh6k.js";e.lazy(()=>s(()=>import("./Article-3xyu56yz.js"),__vite__mapDeps([0,1,2,3])));function a(){return r.jsx(r.Fragment,{children:r.jsx(e.Suspense,{fallback:r.jsx("h6",{children:"Loading..."})})})}export{a as default};
