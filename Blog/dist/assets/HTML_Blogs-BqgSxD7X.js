bf6d20ee6ab0c62e252ff0ba0b6deab90861ecbf
Yll Kelmendi <yllkelmendi05@gmail.com>
2024-05-14 16:34:20 +0200

    deleted:    Blog/dist/Fonts/Jersey15-Regular.ttf 	new file:   Blog/dist/Fonts/Jersey15-Regular.woff2 	modified:   Blog/dist/Images/apple_logo.svg 	modified:   Blog/dist/Images/css_logo.svg 	modified:   Blog/dist/Images/cybertruck_logo.svg 	modified:   Blog/dist/Images/html_logo.svg 	modified:   Blog/dist/Images/javascript_logo.svg 	modified:   Blog/dist/Images/notion_logo.svg 	modified:   Blog/dist/Images/react_logo.svg 	modified:   Blog/dist/Images/webDev_logo.svg 	renamed:    Blog/dist/assets/Article-ByHJl6rX.js -> Blog/dist/assets/Article-Cidb3LnW.js 	renamed:    Blog/dist/assets/ArticleChapterBar-tJQ7OBqj.js -> Blog/dist/assets/ArticleChapterBar-C8FDT2UZ.js 	renamed:    Blog/dist/assets/Blog-Ffl1h-y8.js -> Blog/dist/assets/Blog-CWkSAXDt.js 	renamed:    Blog/dist/assets/BlogsToLoad-BlOY70pj.js -> Blog/dist/assets/BlogsToLoad-ij-Y1FCF.js 	deleted:    Blog/dist/assets/CSS_Blogs-BpN4ICkW.js 	new file:   Blog/dist/assets/CSS_Blogs-BqgSxD7X.js 	renamed:    Blog/dist/assets/Contact-D8XoKovg.js -> Blog/dist/assets/Contact-CYEVJ3Lw.js 	renamed:    Blog/dist/assets/Footer-hjOCsH-D.js -> Blog/dist/assets/Footer-xEZ0j5gU.js 	deleted:    Blog/dist/assets/HTML_Blogs-BpN4ICkW.js 	new file:   Blog/dist/assets/HTML_Blogs-BqgSxD7X.js 	renamed:    Blog/dist/assets/Helmet-i7Iedzp7.js -> Blog/dist/assets/Helmet-ChXYgtGO.js 	renamed:    Blog/dist/assets/Home-BiXfoOqG.js -> Blog/dist/assets/Home-CgxgOztB.js 	renamed:    Blog/dist/assets/How_To_Start_Web_Development_as_a_beginner-C4xFHFP7.js -> Blog/dist/assets/How_To_Start_Web_Development_as_a_beginner-5h9j91KF.js 	deleted:    Blog/dist/assets/JavaScript_Blogs-CaUn6RAT.js 	new file:   Blog/dist/assets/JavaScript_Blogs-MopAZWwt.js 	renamed:    Blog/dist/assets/MainPageContent-CRta1are.js -> Blog/dist/assets/MainPageContent-DEdlH245.js 	renamed:    Blog/dist/assets/Navbar-BXl_fcMn.js -> Blog/dist/assets/Navbar-BadTlCfL.js 	renamed:    Blog/dist/assets/Other_Blogs-CM6ANHOH.js -> Blog/dist/assets/Other_Blogs-C0ur-j4M.js 	renamed:    Blog/dist/assets/Page404-C-9yJz0_.js -> Blog/dist/assets/Page404-Cy4pWZ-h.js 	new file:   Blog/dist/assets/React_Blogs-BTEEzjtf.js 	deleted:    Blog/dist/assets/React_Blogs-C8DKzHbY.js 	renamed:    Blog/dist/assets/RecentBlogs-D_KNhdc4.js -> Blog/dist/assets/RecentBlogs-nLHhcWud.js 	renamed:    Blog/dist/assets/ThreeLines-PEYw6FE1.js -> Blog/dist/assets/ThreeLines-D4_2Yd8T.js 	renamed:    Blog/dist/assets/WebsiteInspo-BxTrjgbB.js -> Blog/dist/assets/WebsiteInspo-CFIrTm5d.js 	new file:   Blog/dist/assets/index-B9hIzNbL.css 	renamed:    Blog/dist/assets/index-DALZj6li.js -> Blog/dist/assets/index-BaXgdXvm.js 	deleted:    Blog/dist/assets/index-CuzwPZim.css 	modified:   Blog/dist/index.html

---
+++ Blog/dist/assets/HTML_Blogs-BqgSxD7X.js
@@ -0,0 +1,2 @@
+const __vite__fileDeps=["assets/Article-Cidb3LnW.js","assets/index-BaXgdXvm.js","assets/index-B9hIzNbL.css","assets/Article-UjdH0_Ib.css"],__vite__mapDeps=i=>i.map(i=>__vite__fileDeps[i]);
+import{j as r,r as e,_ as s}from"./index-BaXgdXvm.js";e.lazy(()=>s(()=>import("./Article-Cidb3LnW.js"),__vite__mapDeps([0,1,2,3])));function a(){return r.jsx(r.Fragment,{children:r.jsx(e.Suspense,{fallback:r.jsx("h6",{children:"Loading..."})})})}export{a as default};
--- Blog/dist/assets/React_Blogs-C8DKzHbY.js
+++
@@ -1,2 +0,0 @@
-const __vite__fileDeps=["assets/Article-ByHJl6rX.js","assets/index-DALZj6li.js","assets/index-CuzwPZim.css","assets/Article-UjdH0_Ib.css"],__vite__mapDeps=i=>i.map(i=>__vite__fileDeps[i]);
-import{j as r,r as e,_ as t}from"./index-DALZj6li.js";e.lazy(()=>t(()=>import("./Article-ByHJl6rX.js"),__vite__mapDeps([0,1,2,3])));function s(){return r.jsx(r.Fragment,{children:r.jsx(e.Suspense,{fallback:r.jsx("h6",{children:"Loading..."})})})}export{s as default};
